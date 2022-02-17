77dd9c78a1ec0996bca2a7890eb4d3634d96bc03
Johny W. Alves <johnywalves@gmail.com>
2022-02-17 11:06:43 -0300

    :sparkles: Adicionar rotas

---
+++ src/routes/Medals.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Medals = () => {
+    return <h1>Medalhas</h1>
+}
+
+export default Medals
---
+++ src/routes/Teams.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Teams = () => {
+    return <h1>Time</h1>
+}
+
+export default Teams
