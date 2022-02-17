fae55fbf811aa6f0bb1d83420c203ab3ae99bab8
Johny W. Alves <johnywalves@gmail.com>
2022-02-17 11:40:31 -0300

    :sparkles: Adição de header e footer

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import {
   Routes
 } from "react-router-dom";
 
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+
 import Medals from './routes/Medals'
 import Teams from './routes/Teams'
 
 function App() {
   return (
     <Router>
-        <div>
-          <Routes>
-            <Route path="/:team" element={<Teams />}/>
-            <Route path="/" element={<Medals />}/>
-          </Routes>
-        </div>
+      <Header/>
+      <Main>
+        <Routes>
+          <Route path="/:team" element={<Teams />}/>
+          <Route path="/" element={<Medals />}/>
+        </Routes>
+      </Main>
+      <Footer/>
     </Router>
   );
 }
