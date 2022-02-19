157f3d73160f4fe036d08cd767ba71e48a99cb88
Johny W. Alves <johnywalves@gmail.com>
2022-02-19 11:50:20 -0300

    :alien: Criação e aplicação de fake API

--- src/routes/Medals/index.jsx
+++ src/routes/Medals/index.jsx
@@ -1,53 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Header from './Header'
 import Row from './Row'
 
 import { Wrapper } from './styles'
 
-const teams = [  
-    {
-        team: 'norway',
-        description: 'Norway',
-        gold: 14,
-        silver: 7,
-        bronze: 8
-    },
-    {
-        team: 'germany',
-        description: 'Germany',
-        gold: 10,
-        silver: 7,
-        bronze: 5
-    },
-    {
-        team: 'united-states-of-america',
-        description: 'United States of America',
-        gold: 8,
-        silver: 8,
-        bronze: 5
-    },
-    {
-        team: 'sweden',
-        description: 'Sweden',
-        gold: 7,
-        silver: 4,
-        bronze: 4
-    },
-    {
-        team: 'china',
-        description: 'People`s Republic of China',
-        gold: 7,
-        silver: 4,
-        bronze: 2
-    }                
-]
-
 const Medals = () => {
+    const [medals, setMedals] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8000/medals')
+            .then(response => response.json())
+            .then(data => setMedals(data));
+    }, [])
+
     return (
         <Wrapper>
             <Header/>
-            {teams.map(team => <Row key={team.team} {...team}/>)}
+            {medals.map(team => <Row key={team.team} {...team}/>)}
         </Wrapper>
     )
 }
--- src/routes/Teams/index.jsx
+++ src/routes/Teams/index.jsx
@@ -1,54 +1,32 @@
-import React, { useMemo } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
 
 import { Wrapper, Flag, Name, Description } from './styles'
 
-const teams = [  
-    {
-        id: 'norway',
-        name: 'Norway',
-        description: 'The Norwegian flag is a white-fimbriated blue Nordic cross on a red field. The used colors in the flag are blue, red, white. The proportion of the Norwegian flag is 8:11. The Flag of Norway was adopted in 1821. The first use of the current flag design was in 1821. The last change to the current Norwegian flag design was in 1899.'
-    },
-    {
-        id: 'germany',
-        name: 'Germany',
-        description: 'The German flag is a horizontal triband. The used colors in the flag are red, yellow, black. The proportion of the German flag is 3:5. The Flag of Germany was adopted in 1918. The first use of the current flag design was in 1848. The last change to the current German flag design was in 1949.'
-    },
-    {
-        id: 'united-states-of-america',
-        name: 'United States of America',
-        description: 'The United States flag – USA flag – American flag is a flag with thirteen horizontal stripes with 50 white stars in a blue field. The used colors in the flag are blue, red, white. The proportion of the United States flag – USA flag – American flag is 10:19. The Flag of the United States was adopted in 1960. The first use of the current flag design was in 1818. The last change to the current United States flag design was in 1960.'
-    },
-    {
-        id: 'sweden',
-        name: 'Sweden',
-        description: 'The Swedish flag is a one coloured field with a cross. The used colors in the flag are blue, yellow. The proportion of the Swedish flag is 5:8. The Flag of Sweden was adopted in 1906. The first use of the current flag design was in 1562. The last change to the current Swedish flag design was in 1906.'
-    },
-    {
-        id: 'china',
-        name: 'People`s Republic of China',
-        description: 'The Chinese flag is a solid flag with four small stars and one bigger star in the upper left corner. The used colors in the flag are red, yellow. The proportion of the Chinese flag is 2:3. The Flag of China was adopted in 1949. The first use of the current flag design was in 1949.'
-    }                
-]
-
 const Teams = () => {
     const { team } = useParams();
 
-    const { id, name, description } = useMemo(() =>  {
-        const current = teams.find(({ id: id_team }) => id_team === team)
+    const [current, setCurrent] = useState()
 
-        if (current) { 
-            return current
-        }
-
-        return ({ id: '', name: '', description: '' })
+    useEffect(() => {
+        fetch(`http://localhost:8000/teams?id=${team}`)
+            .then(response => response.json())
+            .then(data => data && data.length > 0 && setCurrent(data[0]));
     }, [team])
+
+    if (!current) {
+        return (
+            <Wrapper>
+                <Name>Time não encontrado</Name>
+            </Wrapper>
+        )
+    }
 
     return (
         <Wrapper>
-            <Name>{name}</Name>
-            <Flag id={id}/>
-            <Description>{description}</Description>
+            <Name>{current.name}</Name>
+            <Flag id={current.id}/>
+            <Description>{current.description}</Description>
         </Wrapper>
     )
 }
